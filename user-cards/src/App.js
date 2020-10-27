8817a0bcb62637ba0e492d4848f1fc7dabf5b063
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-27 11:59:25 -0700

    Fetching followers and rendering data

--- user-cards/src/App.js
+++ user-cards/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios'
 import './App.css';
 import UserCard from './components/UserCard';
+import Followers from './components/Followers';
 
 
 class App extends React.Component {
@@ -25,8 +26,11 @@ class App extends React.Component {
   }
   render() { 
     return ( 
-      <div>
+      <div className='app'>
+        <h2>Main User: {this.state.data.login}</h2>
         <UserCard user={this.state.data} />
+    <h2>{this.state.data.login}'s Followers</h2>
+        <Followers />
       </div>
      );
   }
